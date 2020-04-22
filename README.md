921fc4b703dec56e88c2feba84bf8c5c2cf6c62b
Michael Schwarz <michael.schwarz@iaik.tugraz.at>
2020-04-22 12:25:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,126 @@
+# LabRunner 
+
+A tool for transferring files and running commands on multiple remote machines. 
+This is commonly needed in testing environments (labs), where one command or tool has to be run on multiple different machines in the network. 
+
+# Installation
+
+LabRunner is written in Python 3 and has the following dependencies: `paramiko`, `pyyaml`, `click`, and `colorlog`. They can simply be installed by running
+
+    pip3 install -r requirements.txt
+
+
+# Usage
+
+LabRunner takes a YAML configuration file that defines the available machines in the lab. Each machine has a name, location, authentication information, and a list of groups it belong to. 
+A simple use of LabRunner to get the CPU info of all machines defined in the configuration file is
+
+    python3 labrun.py --run "cat /proc/cpuinfo" --all
+
+## Running Commands
+Commands are specified using the `--run` (or short `-r`) option. It is possible to run multiple commands by specifying the `--run` parameter multiple times, e.g. `--run "chmod +x ./test.sh" --run "./test.sh"`. The commands are executed sequentially in the order they are defined on the command line. 
+
+## Transferring Files
+LabRunner also supports transferring files before (upload) and after (download) running commands. 
+To upload a file to the remote machine, use the `--copy` (short short `-c`) option. Similar to commands, it is possible to upload multiple files by specifying the option multiple times. All files are uploaded to the default working directory of ssh, which is usually the home folder of the user. 
+To download files, e.g., results, after running the command(s), use the `--get` (or short `-g`) option. Again, specifying the option multiple times allows downloading multiple files. 
+If you want to delete all uploaded files when everything is finished, you can specifiy the `--delete` (or short `-d`) option. 
+
+## Filtering Machines
+LabRunner can target all machines, a single machine, or a subset of machines. 
+With the `--all` (or short `-a`) option, LabRunner targets all machines defined in the configuration file. 
+If you want to target only one machine, use the `--machine` (or short `-m`) option followed by the name of the machine. 
+The `--machine` option can also be used multiple times to select multiple machines, e.g., `-m lab01 -m lab02`. 
+Machines can also be part of groups defined in the configuration file (like tags). 
+Use the `--group` (or short `-G`) to select all machines in a specific group, e.g. `-m x86`. 
+
+## Parallelize
+By default, LabRunner connects to the machines sequentially to execute the commands. 
+However, LabRunner also supports a parallel mode with the `--parallelize` (or short `-p`) option. 
+In parallel mode, LabRunner connects to as many machines simultaneously as there are available CPU cores. 
+While this usually results in faster overall execution time if there are multiple target machines, the order of the output is not sequential anymore. 
+Hence, if you use parallel mode, it is recommended to also use the `--save-output` (or short `-S`) option which saves the standard output of every remote machine in its own file `<machine name>.stdout`. 
+
+## Options
+LabRunner supports the following options
+
+| Parameter | Description |
+|--|--|
+| -c FILE / --copy FILE | Copy file FILE to remote machine. |
+| -r CMD / --run CMD | Runs the command CMD on the remote machine. |
+| -g FILE / --get FILE | Downloads the file FILE from the remote machine. |
+| -d / --delete | Deletes the files copied to the machine after running the commands and downloading files. |
+| -S / --save-output | Saves the stdout as a file. |
+| -q / --quiet | Do not display stdout on terminal. |
+| -G GROUP / --group GROUP | The group of machines to run on. |
+| -m MACHINE / --machine MACHINE | The machine to run on. |
+| -a / --all | Run on all machines. |
+| -s / --simulate | Simulate only, do not connect to any machine. |
+| -v / --verbose | Show debug information. |
+| -M FILE / --machine-list FILE | The YAML file containing the remote-machine configurations (default: machines.yaml). |
+| -A FILE / --auth FILE | The YAML file containing the authentication details including proxies (default auth.yaml). |
+| -p / --parallelize | Connect to remote machines in parallel instead of sequentially. |
+
+# File formats
+Both the machine configuration (machines.yaml) and authentication settings (auth.yaml) are YAML files. 
+
+## Machines.yaml
+This file contains the configuration of all remote machines. Every entry looks similar to the following.
+
+    - machine:
+        name: lab01
+        auth: *labauth
+        server: lab01.testing.company.com
+        sets: 
+            - x86
+            - linux
+            - experiment1
+            
+            
+* `name` is the machine name which is used for filtering (the `-m` option) and as a prefix when download files or storing the standard output. Thus, it should only contain alphanumeric characters and no spaces. 
+* `auth` is a reference to an authentication information in the authentication settings (auth.yaml)
+* `server` is the domain/IP of the machine
+* `sets` is an optional list of groups that this machine is part of. Groups can be seen like tags. They are only used for filtering (with the `-G` option). 
+* `timeout` is an optional timeout in seconds (default: 5) after which the connection has to be established.
+* `port` (optional) specifies the SSH port of the machine (default: 22)
+
+## Auth.yaml
+This file contains the authentication information for the machines. There are two different types of entries in there, `auth` and `proxy`. 
+
+### auth
+The `auth` entry contains the actual authentication information. Authentication is supported via username/password and via keys. An entry looks as follows.
+
+    - auth: &labauth
+        username: <your username>
+        password: <your password>
+        proxy: *proxy
+
+The identifier in the first line (here: `&labauth`) is required, as this is used in the machine configuration (machines.yaml) to reference the authentication information. 
+
+The following fields are supported:
+
+* `username` (required) is the username to use for the remote connection
+* `password` (optional) if login via username/password is used, specify a password
+* `keyfile` (optional) if login via key, specify the path to the key file here
+* `key` (optional) if login via key, you can alternatively enter the private key here
+* `proxy` (optional) if a proxy is required, i.e., whether LabRunner has to connect to a different SSH machine first
+
+### proxy
+The `proxy` entry contains an SSH machine from which the connection to the remote machines can be established (typically defined as `ProxyCommand` in the ssh config).
+An entry looks as follows.
+
+    - proxy: &proxy
+        username: <your username>
+        password: <your password>
+        server: jump.testing.company.com
+        
+The identifier in the first line (here: `&proxy`) is required, as this is used in `auth` entries to reference the proxy. 
+
+The following fields are supported:
+
+* `server` (required) the proxy server's domain/IP
+* `username` (required) is the username to use for the proxy server
+* `password` (optional) if login via username/password is used, specify a password
+* `keyfile` (optional) if login via key, specify the path to the key file here
+* `key` (optional) if login via key, you can alternatively enter the private key here
+* `timeout` (optional) timeout in seconds (default: 5) after which the connection has to be established.
---
+++ labrun.py
@@ -0,0 +1,257 @@
+import paramiko
+import yaml
+import sys
+import time
+import socket     
+from select import select
+import click
+import logging
+from colorlog import ColoredFormatter
+import os
+from multiprocessing import Pool, TimeoutError
+
+
+logger = None
+
+def setup_logger():
+    formatter = ColoredFormatter(
+        "%(log_color)s%(levelname)-8s%(reset)s %(white)s%(message)s",
+        datefmt=None,
+        reset=True,
+        log_colors={
+            'DEBUG':    'cyan',
+            'INFO':     'green',
+            'WARNING':  'yellow',
+            'ERROR':    'red',
+            'CRITICAL': 'red',
+        }
+    )
+
+    logger = logging.getLogger('log')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    return logger
+
+
+class ParaProcess():
+    def __init__(self):
+        self.returncode = 0
+        
+
+class ParaProxy(paramiko.proxy.ProxyCommand):                      
+    def __init__(self, stdin, stdout, stderr):                             
+        self.stdin = stdin                                                 
+        self.stdout = stdout                                               
+        self.stderr = stderr
+        self.timeout = None
+        self.channel = stdin.channel
+        self.process = ParaProcess()
+        
+    def send(self, content):                                               
+        try:                                                               
+            self.stdin.write(content)                                      
+        except IOError as exc:                                             
+            raise socket.error("Error: {}".format(exc))                                                    
+        return len(content)                                                
+
+    def recv(self, size):                                                  
+        try:
+            buffer = b''
+            start = time.time()
+
+            while len(buffer) < size:
+                select_timeout = self._calculate_remaining_time(start)
+                ready, _, _ = select([self.stdout.channel], [], [],
+                                     select_timeout)
+                if ready and self.stdout.channel is ready[0]:
+                      buffer += self.stdout.read(size - len(buffer))
+
+        except socket.timeout:
+            if not buffer:
+                raise
+
+        except IOError as e:
+            return ""
+
+        return buffer
+
+    def _calculate_remaining_time(self, start):
+        if self.timeout is not None:
+            elapsed = time.time() - start
+            if elapsed >= self.timeout:
+                raise socket.timeout()
+            return self.timeout - elapsed
+        return None                                   
+
+    def close(self):                                                       
+        self.stdin.close()                                                 
+        self.stdout.close()                                                
+        self.stderr.close()
+        self.channel.close()
+
+
+class RemoteJob():
+    def __init__(self, machine, copy, run, get, delete, save_output, quiet, simulate, verbose):
+        self.machine = machine
+        self.copy = copy
+        self.run = run
+        self.get = get
+        self.delete = delete
+        self.save_output = save_output
+        self.quiet = quiet
+        self.simulate = simulate
+        self.verbose = verbose
+        
+    def start(self):
+        logger.info("Running job on %s @ %s" % (self.machine["name"], self.machine["server"]))
+
+        auth = self.machine["auth"]
+
+        server = self.machine.get("server", "")
+
+        proxy = auth.get("proxy", None)
+        transport = None
+        try:
+            if proxy:
+                proxy_con = paramiko.SSHClient()
+                proxy_con.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                if not self.simulate:
+                    proxy_con.connect(hostname=proxy["server"], username=proxy.get("username", ""), password=proxy.get("password", None), pkey=proxy.get("key", None), key_filename=proxy.get("keyfile", None), timeout=int(proxy.get("timeout", 5)))
+                    logger.debug("Proxy command 'nc %s %d'" % (server, int(self.machine.get("port", 22))))
+                    nc_con = proxy_con.exec_command("nc %s %d" % (server, int(self.machine.get("port", 22))))
+                    transport = ParaProxy(*nc_con)
+                logger.debug("Connected %s to proxy %s" % (proxy["username"], proxy["server"]))
+
+            username = auth.get("username", "")
+            password = auth.get("password", None)
+            key = auth.get("key", None)
+            keyfile = auth.get("keyfile", None)
+            
+            logger.debug("Connect %s to %s (proxy: %s)" % (username, server, "Yes" if proxy else "No"))
+            
+            if not self.simulate:
+                ssh = paramiko.SSHClient()
+                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                ssh.connect(server, username=username, password=password, pkey=key, key_filename=keyfile, sock=transport, timeout=int(self.machine.get("timeout", 5)))
+            
+            if len(self.copy):
+                if not self.simulate:
+                    ftp_client = ssh.open_sftp()
+                for c in self.copy:
+                    logger.info("Copying '%s' to machine" % c)
+                    if not self.simulate:
+                        ftp_client.put(c, c)
+                if not self.simulate:
+                    ftp_client.close()
+            
+            if len(self.run):
+                stdout_full = []
+                for r in self.run:
+                    if not self.simulate:
+                        _, ssh_stdout, _ = ssh.exec_command(r)
+
+                        stdout = []
+                        for line in ssh_stdout:
+                            stdout.append(line.strip())
+                        stdout_full += stdout
+                            
+                        if not self.quiet: 
+                            logger.info("%s stdout:" % self.machine["name"])
+                            print("\n".join(stdout))
+                if not self.simulate and self.save_output:
+                    with open("%s.stdout" % self.machine["name"], "w") as o:
+                        o.write("\n".join(stdout_full) + "\n")
+                
+            if len(self.get):
+                if not self.simulate:
+                    ftp_client = ssh.open_sftp()
+                for g in self.get:
+                    logger.info("Retrieving '%s'" % g)
+                    if not self.simulate:
+                        ftp_client.get(g, "%s_%s" % (self.machine["name"], g))
+                if not self.simulate:
+                    ftp_client.close()
+                    
+            if len(self.copy) and self.delete:
+                if not self.simulate:
+                    ftp_client = ssh.open_sftp()
+                for c in self.copy:
+                    logger.info("Deleting '%s' from machine" % c)
+                    if not self.simulate:
+                        ftp_client.remove(c)
+                if not self.simulate:
+                    ftp_client.close()   
+        except Exception as e:
+            logger.error("Connection to %s failed!" % self.machine["name"])
+            print(e)
+
+
+def job_runner(job):
+    job.start()
+
+@click.command()
+@click.option('-c', '--copy', default=[], multiple=True, help='Copy file to remote machine.')
+@click.option('-r', '--run', default=[], multiple=True, help='The command to run.')
+@click.option('-g', '--get', default=[], multiple=True, help='Download file from remote machine.')
+@click.option('-d', '--delete', is_flag=True, help='Delete copied files afterwards.')
+@click.option('-S', '--save-output', 'save_output', is_flag=True, help='Save stdout as file.')
+@click.option('-q', '--quiet', is_flag=True, help='Do not display stdout.')
+@click.option('-G', '--group', default=[], multiple=True, help='Group of machines to run on.')
+@click.option('-m', '--machine', default=[], multiple=True, help='Machine to run on.')
+@click.option('-a', '--all', 'all_machines', is_flag=True, help='Run on all machines.')
+@click.option('-s', '--simulate', is_flag=True, help='Simluate only, do not connect.')
+@click.option('-v', '--verbose', is_flag=True, help='Show debug information.')
+@click.option('-M', '--machine-list', 'machine_list', default="machines.yaml", help='YAML file containing the remote-machine configurations.')
+@click.option('-A', '--auth', default="auth.yaml", help='YAML file containing the authentication details (including proxies).')
+@click.option('-p', '--parallelize', is_flag=True, help='Connect to remote machines in parallel.')
+def main(copy, run, get, delete, save_output, quiet, group, machine, all_machines, simulate, verbose, machine_list, auth, parallelize):
+    global logger 
+    logger = setup_logger()
+    if verbose:
+        logger.setLevel(logging.DEBUG)
+    
+    if not os.path.exists(auth):
+        logger.critical("No %s file found! Did you adapt auth.yaml.sample and rename it to auth.yaml?" % auth)
+        sys.exit(1)
+    if not os.path.exists(machine_list):
+        logger.critical("No %s configuration file found" % machine_list)
+        sys.exit(1)
+    
+    config = open(auth).read() + "\n" + open(machine_list).read()
+    setting = yaml.load(config, Loader=yaml.FullLoader)
+    #print(setting["machines"])
+
+    jobs = []
+
+    for mlist in setting["machines"]:
+        m = mlist["machine"]
+        matches = False
+        if m["name"] in machine:
+            matches = True
+        for s in group:
+            if s in m["sets"]:
+                matches = True
+                break
+        if all_machines:
+            matches = True
+        if not matches:
+            continue
+        logger.info("Creating job for %s @ %s" % (m["name"], m["server"]))
+        jobs.append(RemoteJob(m, copy, run, get, delete, save_output, quiet, simulate, verbose))
+
+    logger.info("%d jobs created!" % len(jobs))
+    
+    if parallelize:
+        pool = Pool()
+        pool.map(job_runner, jobs)
+    else:
+        for j in jobs:
+            print("")
+            j.start()
+            
+
+if __name__ == "__main__":
+    main()
